b6ef73f693c536cc0675485bb85c3b9196d47e29
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2025-07-05 09:55:29 +0530

    Reverse Array
    Reverse array throw two pointer approach in c++

--- main (1).cpp
+++ main (1).cpp
@@ -1,24 +1,32 @@
-# include<bits/stdc++.h>
+# include<iostream>
+# include<algorithm>
 using namespace std;
-int repeat(vector<int>&arr){
-        unordered_map<int, int>freq;
-        for( int num :arr){
-            freq[num]++;
-        }
-        for( int num:arr){
-            if(freq[num]==1){
-                return num;
-            }
-        }
-        return 0;
-    }
 int main(){
     int n;
+    cout<<"Enter the size of array";
     cin>>n;
-    vector<int>arr(n);
+    int arr[n];
     for(int i=0;i<n;i++){
-    cin>>arr[i];
+        cin>>arr[i];
     }
-    int result=repeat(arr);
-    cout<<result;
+    
+    cout<<"This is the main array=";
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+        
+    }
+    
+    cout<<"\n";
+  int i=0;
+  int j=n-1;
+  while(i<j){
+      swap(arr[i],arr[j]);
+      i++;
+      j--;
+  }
+  cout<<"This is the reverse array=";
+  
+  for(int i=0;i<n;i++){
+      cout<<arr[i];
+  }
 }
